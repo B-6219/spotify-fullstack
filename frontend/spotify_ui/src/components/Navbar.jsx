eff2be3be3ed41ec28291379d286970ce4f0fe41
B-MICKIE <153866848+B-6219@users.noreply.github.com>
2025-07-14 23:43:26 +0100

    third

---
+++ frontend/spotify_ui/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { assets } from '../assets/frontend-assets/assets'
+
+const Navbar = () => {
+  return (
+    <>
+        <div className='w-full flex justify-between items-center font-semibold'>
+            <div className='flex items-center gap-2'>
+                <img className='w-8 bg-black p-2 rounded-2x1 cursor-pointer' src={assets.arrow_left} alt="" />
+                <img className='w-8 bg-black p-2 rounded-2x1 cursor-pointer' src={assets.arrow_right} alt="" />
+            </div>
+            <div className='fkex items-center gap-4'>
+                <p className='bg-white text-black text-[15px] px-4 py-1 rounded-2x1 md:block cursor-pointer'>Explore Premium</p>
+                <p>Install App</p>
+            </div>
+        </div>
+
+    </>
+  )
+}
+
+export default Navbar
