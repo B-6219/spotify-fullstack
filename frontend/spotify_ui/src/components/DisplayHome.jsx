3eac8a122d29a1a16708fb70f7ab15962f5c4811
B-MICKIE <153866848+B-6219@users.noreply.github.com>
2025-07-16 20:36:35 +0100

    fourth

--- frontend/spotify_ui/src/components/DisplayHome.jsx
+++ frontend/spotify_ui/src/components/DisplayHome.jsx
@@ -1,19 +1,31 @@
 import React from 'react'
 import Navbar from './Navbar'
-import { albumsData } from '../assets/frontend-assets/assets'
+import {albumsData} from '../assets/frontend-assets/assets'
+import AbumItem from './AbumItem.jsx'
+//import {songData} from '../assets/frontend-assets/assets.js'
+
 const DisplayHome = () => {
   return (
         <>
            <Navbar/>
 
-           <div className='my-5 font-bold text-2xl'>
-            {albumsData.map((item,index)=>{
-              <AlbumItem key={index} name ={item.name} desc= {item.desc} id={item.id} image={item.image}/>
-            })}
+           <div className='mb-4'>
+				<h1 className='my-5 font-bold text-2x1'>Featured Charts</h1>
+				<div className='flex overflow-auto'>
+					{albumsData.map((item,index)=>(<AbumItem key={index} name={item.name} desc={item.desc} image={item.image} id={item.id}/>))}
+				</div>
            </div>
-        </>
 
+		    <div className='mb-4'>
+				<h1 className='my-5 font-bold text-2x1'>Biggest Hits</h1>
+				<div className='flex overflow-auto'>
+					{albumsData.map((item,index)=>(<AbumItem key={index} name={item.name} desc={item.desc} image={item.image} id={item.id}/>))}
+				</div>
+            </div>
+
+        </>
+	
   )
-}
+} 
 
 export default DisplayHome
---
+++ frontend/spotify_ui/src/components/SongItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+//const SongItem = (name,image,id,desc) => {
+  //return (
+    //<div>
+
+    //</div>
+ // )
+//}
+
+//export default SongItem
