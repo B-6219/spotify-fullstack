7f89801b8962e029f9393ea4eeb3f741f2d14ae4
B-MICKIE <153866848+B-6219@users.noreply.github.com>
2025-07-17 12:15:55 +0100

    fifth

--- frontend/spotify_ui/src/components/DisplayHome.jsx
+++ frontend/spotify_ui/src/components/DisplayHome.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import Navbar from './Navbar'
 import {albumsData} from '../assets/frontend-assets/assets'
 import AbumItem from './AbumItem.jsx'
-//import {songData} from '../assets/frontend-assets/assets.js'
+import {songsData} from '../assets/frontend-assets/assets.js'
+import SongItem from './SongItem.jsx'
 
 const DisplayHome = () => {
   return (
@@ -13,13 +14,13 @@ const DisplayHome = () => {
 				<h1 className='my-5 font-bold text-2x1'>Featured Charts</h1>
 				<div className='flex overflow-auto'>
 					{albumsData.map((item,index)=>(<AbumItem key={index} name={item.name} desc={item.desc} image={item.image} id={item.id}/>))}
-				</div>
+				 </div>
            </div>
 
 		    <div className='mb-4'>
 				<h1 className='my-5 font-bold text-2x1'>Biggest Hits</h1>
 				<div className='flex overflow-auto'>
-					{albumsData.map((item,index)=>(<AbumItem key={index} name={item.name} desc={item.desc} image={item.image} id={item.id}/>))}
+                    {songsData.map((item,index )=>(<SongItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
 				</div>
             </div>
 
