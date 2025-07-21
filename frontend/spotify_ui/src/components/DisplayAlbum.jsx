12c16ed3ae1492df1900c8db2834a1c70bb466ab
B-MICKIE <153866848+B-6219@users.noreply.github.com>
2025-07-21 23:12:43 +0100

    sixth comit has errors😭😭

--- frontend/spotify_ui/src/components/DisplayAlbum.jsx
+++ frontend/spotify_ui/src/components/DisplayAlbum.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './Navbar'
 import { useParams } from 'react-router-dom'
-import { albumsData, assets } from '../assets/frontend-assets/assets'
+import { albumsData, assets, songsData } from '../assets/frontend-assets/assets'
 
 const DisplayAlbum = () => {
 
@@ -31,7 +31,26 @@ const DisplayAlbum = () => {
 
         <div className='grid grid-cols-3 sm:grid-cols-4 mt-10 mb-4 pl-2 text-[#a7a7a7]'>
             <p><b className='mr-4' >#</b>Title</p>
+            <p>Album</p>
+            <p className='hidden sm:block'>Date added</p>
+            <img className='m-auto w-4' src={assets.clock_icon} alt="" />
         </div>
+
+        <hr />
+        {
+          songsData.map((item,index)=>(
+            <div key={index} className='grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer'>
+              <p className='text-white'>
+                <b className='mr-4 text-[#a7a7a7]'>{index+1}</b>
+                <img className='inline w-10 mr-5' src={item.image} alt="" />
+                {item.name}
+              </p>
+              <p className='text-[15px]'> {albumData.name} </p>
+              <p className='text-[15px] hidden sm:block'>5 days Ago</p>
+              <p className='text-[15px] text-center'>{item.duration}</p>
+            </div>
+          ))
+        }
     </>
   )
 }
--- frontend/spotify_ui/src/components/DisplayHome.jsx
+++ frontend/spotify_ui/src/components/DisplayHome.jsx
@@ -10,18 +10,18 @@ const DisplayHome = () => {
         <>
            <Navbar/>
 
-           <div className='mb-4'>
-				<h1 className='my-5 font-bold text-2x1'>Featured Charts</h1>
-				<div className='flex overflow-auto'>
-					{albumsData.map((item,index)=>(<AbumItem key={index} name={item.name} desc={item.desc} image={item.image} id={item.id}/>))}
-				 </div>
-           </div>
+            <div className='mb-4'>
+              <h1 className='my-5 font-bold text-2x1'>Featured Charts</h1>
+              <div className='flex overflow-auto'>
+                {albumsData.map((item,index)=>(<AbumItem key={index} name={item.name} desc={item.desc} image={item.image} id={item.id}/>))}
+              </div>
+            </div>
 
-		    <div className='mb-4'>
-				<h1 className='my-5 font-bold text-2x1'>Biggest Hits</h1>
-				<div className='flex overflow-auto'>
-                    {songsData.map((item,index )=>(<SongItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
-				</div>
+		        <div className='mb-4'>
+              <h1 className='my-5 font-bold text-2x1'>Biggest Hits</h1>
+              <div className='flex overflow-auto'>
+                  {songsData.map((item,index )=>(<SongItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
+              </div>
             </div>
 
         </>
