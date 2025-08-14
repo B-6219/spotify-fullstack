c6f6b9a52053d1e7d336ed0be1187b0906edbc43
B-MICKIE <153866848+B-6219@users.noreply.github.com>
2025-08-14 09:45:41 +0100

    schema

---
+++ backend/src/models/songModel.js
@@ -0,0 +1,14 @@
+import mongoose, { mongo } from "mongoose";
+
+const songSchema = mongoose.Schema({
+   name:{type:String, require:true},
+   desc:{type:String, require:true},
+   Image:{type:String, require:true},
+   album:{type:String, require:true},
+   file:{type:String, require:true},
+   duration:{type:String, require:true},
+})
+
+const songModel = mongoose.models.song || mongoose.model("song",songSchema)
+
+export default songModel
