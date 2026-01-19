15070c836837dce5cd3d6de6d97b64aa0afcad2a
B-6219 <brayokamau6219@gmail.com>
2026-01-19 12:54:29 -0500

    fixed errors in displayHome

--- frontend/spotify_ui/src/components/DisplayHome.jsx
+++ frontend/spotify_ui/src/components/DisplayHome.jsx
@@ -1,32 +1,66 @@
 import React from 'react'
 import Navbar from './Navbar'
-import {albumsData} from '../assets/frontend-assets/assets'
+import { albumsData } from '../assets/frontend-assets/assets'
 import AbumItem from './AbumItem.jsx'
-import {songsData} from '../assets/frontend-assets/assets.js'
+import { songsData } from '../assets/frontend-assets/assets.js'
+import { artistsData } from '../assets/frontend-assets/assets.js'
 import SongItem from './SongItem.jsx'
+import ArtistItem from './ArtistItem.jsx'
+
 
 const DisplayHome = () => {
   return (
-        <>
-           <Navbar/>
+    <>
+      <Navbar />
 
-            <div className='mb-4'>
-              <h1 className='my-5 font-bold text-2x1'>Featured Charts</h1>
-              <div className='flex overflow-auto'>
-                {albumsData.map((item,index)=>(<AbumItem key={index} name={item.name} desc={item.desc} image={item.image} id={item.id}/>))}
-              </div>
-            </div>
+      {/* Featured Charts */}
+      <section className="mb-12">
+        <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+        <div className="flex gap-4 overflow-x-auto">
+          {albumsData.map(item => (
+            <AbumItem
+              key={item.id}
+              name={item.name}
+              desc={item.desc}
+              image={item.image}
+              id={item.id}
+            />
+          ))}
+        </div>
+      </section>
 
-		        <div className='mb-4'>
-              <h1 className='my-5 font-bold text-2x1'>Biggest Hits</h1>
-              <div className='flex overflow-auto'>
-                  {songsData.map((item,index )=>(<SongItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
-              </div>
-            </div>
+      {/* Popular Artists */}
+      <section className="mb-12">
+        <h1 className="my-5 font-bold text-2xl">Popular Artists</h1>
+        <div className="flex gap-4 overflow-x-auto">
+          {artistsData.map(item => (
+            <ArtistItem
+              key={item.id}
+              name={item.name}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </section>
 
-        </>
-	
+      {/* Popular Songs */}
+      <section className="mb-12">
+        <h1 className="my-5 font-bold text-2xl">Popular Songs</h1>
+        <div className="flex gap-4 overflow-x-auto">
+          {songsData.map(item => (
+            <SongItem
+              key={item.id}
+              name={item.name}
+              desc={item.desc}
+              image={item.image}
+              id={item.id}
+            />
+          ))}
+        </div>
+      </section>
+    </>
   )
-} 
+}
+
 
 export default DisplayHome
